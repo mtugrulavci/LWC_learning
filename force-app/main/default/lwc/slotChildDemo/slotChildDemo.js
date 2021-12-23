1a623cdd05bd2c818a49b288268d83243f3521e2
Mustafa Tugrul Avci <mtavci@yorku.ca>
2021-12-22 23:56:26 -0500

    slot element

--- force-app/main/default/lwc/slotChildDemo/slotChildDemo.js
+++ force-app/main/default/lwc/slotChildDemo/slotChildDemo.js
@@ -2,6 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class SlotChildDemo extends LightningElement {
     handleFooterChange(){
+        
         const footerElem =this.template.querySelector('.slds-card__footer')
         if(footerElem){
             footerElem.classList.remove('slds-hide')

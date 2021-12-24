1a135428e3635d8a9b8ba831850600fa7c49613a
Mustafa Tugrul Avci <mtavci@yorku.ca>
2021-12-23 23:35:07 -0500

    lwc -Aura communication

---
+++ force-app/main/default/lwc/lwcAuraCommunication/lwcAuraCommunication.js
@@ -0,0 +1,11 @@
+import { LightningElement, api } from 'lwc';
+
+export default class LwcAuraCommunication extends LightningElement {
+    @api title
+    callAura(){
+       const event =  new CustomEvent('sendmsg', {detail:{
+            "msg":" hello from LWC"}
+        })
+        this.dispatchEvent(event)
+    }
+}

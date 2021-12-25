0fc84828c086c07bfb4d3b0050402dce44bb200a
Mustafa Tugrul Avci <mtavci@yorku.ca>
2021-12-24 19:26:41 -0500

    Subscribtion added

--- force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
+++ force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
@@ -1,18 +1,25 @@
 import { LightningElement, wire } from 'lwc';
 import SAMPLEMC from "@salesforce/messageChannel/SampleMessageChannel__c"
-import { subscribe,MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
+import { subscribe,MessageContext, APPLICATION_SCOPE, unsubscribe } from 'lightning/messageService';
 export default class LmsComponentX extends LightningElement {
    recievedMessage
     @wire(MessageContext)
     context
+    subscription
 connectedCallback(){
     this.subscribeMessage()
 }
 
 subscribeMessage(){
-    subscribe(this.context, SAMPLEMC, (message)=>{this.handleMessage(message)},{scope:APPLICATION_SCOPE})
+    this.subscription = subscribe(this.context, SAMPLEMC, (message)=>{this.handleMessage(message)},{scope:APPLICATION_SCOPE})
 }
 handleMessage(message){
     this.recievedMessage =message.lmsData.value? message.lmsData.value:"No Message published"
+}
+
+unsubscribeMessage(){
+    unsubscribe(this.subscription)
+    this.subscription =null
+
 }
 }

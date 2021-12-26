73537b33bd7b109fa64b2973291c4149f336bf7f
Mustafa Tugrul Avci <mtavci@yorku.ca>
2021-12-26 15:45:57 -0500

    Custom Labels are added

---
+++ force-app/main/default/lwc/customLabelsDemo/customLabelsDemo.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+import DESCRIPTION_ONE from '@salesforce/label/c.descriptionOne'
+import DESCRIPTION_TWO from '@salesforce/label/c.descriptionTwo'
+export default class CustomLabelsDemo extends LightningElement {
+
+LABELS ={
+    descOne:DESCRIPTION_ONE,
+    descTwo:DESCRIPTION_TWO
+}
+
+}

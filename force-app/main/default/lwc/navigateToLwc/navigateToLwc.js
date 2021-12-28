a65699775bcac63fa84f625e9863ed3a0817bc7b
Mustafa Tugrul Avci <mtavci@yorku.ca>
2021-12-28 17:06:03 -0500

    Navigation to LWC Component

---
+++ force-app/main/default/lwc/navigateToLwc/navigateToLwc.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+import {NavigationMixin} from 'lightning/navigation'
+export default class NavigateToLwc extends NavigationMixin(LightningElement) {
+
+    navigateToLWC(){
+        var defination = {
+            componentDef:'c:navigationLwcTarget'
+        }
+       this[NavigationMixin.Navigate]({
+           type:'standard__webPage',
+           attributes:{
+               url:'/one/one.app#' +btoa(JSON.stringify(defination))
+           }
+       }) 
+    }
+
+}

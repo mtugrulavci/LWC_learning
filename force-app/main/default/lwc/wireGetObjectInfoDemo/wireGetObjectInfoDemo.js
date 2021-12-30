ae8fcf7f88794566c1341aeae47fc8f7289d4bb7
Mustafa Tugrul Avci <mtavci@yorku.ca>
2021-12-29 22:44:19 -0500

    wire service get object Info

---
+++ force-app/main/default/lwc/wireGetObjectInfoDemo/wireGetObjectInfoDemo.js
@@ -0,0 +1,12 @@
+import { LightningElement,wire } from 'lwc';
+import {getObjectInfo} from 'lightning/uiObjectInfoApi'
+import ACCOUNT_OBJECT from "@salesforce/schema/Account"
+
+
+export default class WireGetObjectInfoDemo extends LightningElement {
+    
+    @wire(getObjectInfo, {objectApiName:ACCOUNT_OBJECT})
+    objectInfo
+
+    
+}
